595f68cad857d02b8e3684d288050110aa5af435
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-12-18 16:37:40 -0700

    added comments and another stretch goal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+//get the React library from the dependencies
 import React, { Component } from 'react';
+//get the loader from the npm package
 import Loader from 'react-loader-spinner'
 import './App.css';
 
+//instantiating the class that extends the React Component 
 class App extends Component {
+
   render() {
+
+    //what we are returning in the render function
     return (
       <div className="App">
       <h1>I find your lack of fetch disturbing..</h1>
